b0da582b59cf336df828f13000866a370e8eaf55
nor2101 <61382188+nor2101@users.noreply.github.com>
2023-05-25 13:48:04 +0200

    Changed credits

--- libs/sensors.h
+++ libs/sensors.h
@@ -1,6 +1,6 @@
 /*
                         Milano Smart Park Firmware
-                      Copyright (c) Norman Mulinacci
+                       Developed by Norman Mulinacci
 
           This code is usable under the terms and conditions of the
              GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007
